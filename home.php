a2669238861b1157cea08576e7ac2ad705512476
pejade14 <48130961+pejade14@users.noreply.github.com>
2019-03-01 23:35:05 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title> Home Page </title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">﻿
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<nav>
+		<a href="http://localhost:82/RKT/index.php"><img src="pj.png" class="img"></a>
+			<ul>
+				<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">Contact Us</a></li>
+			</ul>
+			</nav>
+	</div>
+	<div class="main">
+	<form class="box">
+				<label>Email Address:</label></br>
+				<input type="text" placeholder="Enter Email Address..."></br>
+				<label>Password:</label></br>
+				<input type="password" placeholder="Enter Password..."></br>
+				<input type="submit" value="Login">
+				</form>
+				</div>
+	
+	</div>
+	<div class="footer">
+	<div class="footer-top">
+<div class="menu">
+		<ul>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+		</div>
+		<div class="icons">
+		<ul>
+			<li><a href="https://www.facebook.com" target="blank"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="https://twitter.com" target="blank"><i class="fa fa-twitter"></i></a></li>
+			<li><a href="https://youtube.com" target="blank"><i class="fa fa-youtube"></i></a></li>
+		</ul>
+		</div>
+		</div>
+	<div class="footer-bot">
+	<p>DEVELOPED BY:  <a href="https://www.facebook.com/akosipejade14">PETER JADE M. DELA VEGA </a></p>
+	</div>
+</div>
+</body>
+</html>
